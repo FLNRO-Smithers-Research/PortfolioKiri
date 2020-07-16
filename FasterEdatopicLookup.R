5625c7be52fda32eddb5908d09f8ce5b181f294a
Kiri Daust <34073038+kdaust@users.noreply.github.com>
2020-07-16 12:03:58 -0700

    Added special edatopic match

--- FasterEdatopicLookup.R
+++ FasterEdatopicLookup.R
@@ -1,10 +1,27 @@
 BGC <- as.data.table(Y3.sub1)
+Edatope <- as.data.table(Edatope)
 SS <- as.data.table(Edatope)
 SS <- SS[is.na(Special),.(BGC,SS_NoSpace,Edatopic)]
 SS <- unique(SS)
 BGC <- unique(BGC)
 BGC <- BGC[,-c("BGC.len","Pred.len")]
+SSsp <- Edatope[!is.na(Codes),.(BGC,SS_NoSpace,Codes)]
+SSsp <- unique(SSsp)
 
+##Special edatopes
+CurrBGC <- SSsp[BGC, on = "BGC", allow.cartesian = T]
+setkey(BGC, BGC.pred)
+setkey(SSsp, BGC)
+FutBGC <- SSsp[BGC, allow.cartesian = T]
+setnames(FutBGC, old = c("BGC","SS_NoSpace","i.BGC"), 
+         new = c("BGC.pred","SS.pred","BGC"))
+FutBGC <- FutBGC[!is.na(SS.pred),]
+setkey(FutBGC, SiteNo, FuturePeriod, BGC,BGC.pred, Codes)
+setkey(CurrBGC,SiteNo,FuturePeriod, BGC,BGC.pred, Codes)
+new <- CurrBGC[FutBGC]
+SSsp.out <- new[,.(allOverlap = 1/.N,SS.pred,BGC.prop), keyby = .(SiteNo,FuturePeriod,BGC,BGC.pred,SS_NoSpace)]
+
+##regular
 CurrBGC <- SS[BGC, on = "BGC", allow.cartesian = T]
 setkey(BGC, BGC.pred)
 setkey(SS, BGC)
@@ -36,17 +53,15 @@ comb[,SSProb := SS.prob/SS.Curr]
 combRev[,SSProbRev := SS.prob/SS.Curr]
 combAll <- merge(comb,combRev,by = c("SiteNo","FuturePeriod","BGC","BGC.pred","SS_NoSpace","SS.pred"))
 combAll[,allOverlap := SSProb*SSProbRev]
+setnames(combAll, old = "BGC.1.x",new = "BGC.prop")
+combAll <- combAll[,.(SiteNo, FuturePeriod, BGC, BGC.pred, SS_NoSpace, 
+                      allOverlap, SS.pred, BGC.prop)]
+combAll <- rbind(combAll, SSsp.out)
 
 ##maybe need to recalculate BGC prob?
 combAll[,SSratio := allOverlap/sum(allOverlap), by = .(SiteNo, FuturePeriod, BGC, BGC.pred,SS_NoSpace)]
-
-
-dat <- fread(file.choose())
-dat[,ID := seq(nrow(dat))]
-dat <- data.table::melt(dat, id.vars = "ID")
-setnames(dat, c("ID","Model","Response"))
-dat3 <- dat[,.(NumMods = length(unique(Response))), by = ID]
-
-dat2 <- dat[,.(Num = .N), by = .(ID, Response)]
-dat2 <- dat2[,.(MaxNum = max(Num)), by = ID]
-dat2[,PropSame := MaxNum/4]
+temp <- unique(combAll[,.(SiteNo,FuturePeriod,BGC,BGC.pred,BGC.prop)])
+temp[,BGC.prop := BGC.prop/sum(BGC.prop), by = .(SiteNo,FuturePeriod,BGC)]
+combAll[,BGC.prop := NULL]
+combAll <- temp[combAll, on = c("SiteNo","FuturePeriod","BGC","BGC.pred")]
+combAll[,SSprob := SSratio*BGC.prop]
